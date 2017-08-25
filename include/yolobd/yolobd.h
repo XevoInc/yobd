85a711ccd372c9ae46d5822cee28e6fc144e3849
Martin Kelly <mkelly@xevo.com>
2017-08-25 14:39:44 -0700

    add parser stub
    All the build logic is here, but no real code.

---
+++ include/yolobd/yolobd.h
@@ -0,0 +1,14 @@
+/**
+ * @file      yolobd.h
+ * @brief     YolOBD public header.
+ * @author    Martin Kelly <mkelly@xevo.com>
+ * @copyright Copyright (C) 2017 Xevo Inc. All Rights Reserved.
+ */
+
+#ifndef YOLOBD_H_
+#define YOLOBD_H_
+
+/* Forward declaration. */
+struct yolobd_ctx;
+
+#endif /* YOLOBD_H_ */
---
+++ src/parser.c
@@ -0,0 +1,4 @@
+#include <stdint.h>
+
+typedef uint_fast8_t mode;
+typedef uint_fast16_t pid;
