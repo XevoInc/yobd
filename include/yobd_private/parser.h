af5588e8c0accb53b4621a2dfc9dca6176eca019
Martin Kelly <mkelly@xevo.com>
2017-09-07 14:59:02 -0700

    switch to new xlib from klib

--- include/yobd_private/parser.h
+++ include/yobd_private/parser.h
@@ -1,8 +1,8 @@
 #ifndef YOBD_PRIVATE_PARSER_H_
 #define YOBD_PRIVATE_PARSER_H_
 
-#include <klib/khash.h>
 #include <stdint.h>
+#include <xlib/xhash.h>
 #include <yobd/yobd.h>
 
 typedef enum {
@@ -24,15 +24,15 @@ struct parse_pid_ctx *get_parse_ctx(
     yobd_mode mode,
     yobd_pid pid);
 
-KHASH_MAP_INIT_INT8(UNIT_MAP, const char *)
-KHASH_MAP_INIT_INT(MODEPID_MAP, struct parse_pid_ctx)
-KHASH_SET_INIT_STR(STR_SET)
+XHASH_MAP_INIT_INT8(UNIT_MAP, const char *)
+XHASH_MAP_INIT_INT(MODEPID_MAP, struct parse_pid_ctx)
+XHASH_SET_INIT_STR(STR_SET)
 
 struct yobd_ctx {
     yobd_unit next_unit_id;
     bool big_endian;
-    khash_t(UNIT_MAP) *unit_map;
-    khash_t(MODEPID_MAP) *modepid_map;
+    xhash_t(UNIT_MAP) *unit_map;
+    xhash_t(MODEPID_MAP) *modepid_map;
 };
 
 #endif /* YOBD_PRIVATE_PARSER_H_ */
